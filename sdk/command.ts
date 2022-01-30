42a54cd33fb8267e4610695cd5f3a68714b1e948
Andrei Zbikowski <b1naryth1ef@users.noreply.github.com>
2022-01-30 02:43:28 -0800

    fix: addSubMenu reversed lol

--- sdk/command.ts
+++ sdk/command.ts
@@ -32,8 +32,8 @@ export function addSubMenu(
   target?: GroupOrCoalition,
 ): Promise<{ path: Array<string> }> {
   return runTask("missionCommandsAddSubMenu", {
-    name: path[0],
-    path: path.slice(1),
+    name: path[path.length - 1],
+    path: path.length > 1 ? path.slice(0, path.length - 1) : null,
     target,
   });
 }
